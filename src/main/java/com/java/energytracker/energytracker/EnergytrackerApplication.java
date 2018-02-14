9e864d4e5870fb43c1a6aea77108e9e28ecae61b
Vijay Shankar Ketha <vijayshankar.ketha@gmail.com>
2018-02-14 06:34:52 +0530

    Defining the Architecture as a first step Used Structurizr framework to code the Architecture Completed Context and Container diagrams... Component diagram to follow

---
+++ src/main/java/com/java/energytracker/energytracker/EnergytrackerApplication.java
@@ -0,0 +1,12 @@
+package com.java.energytracker.energytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergytrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergytrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/java/energytracker/energytracker/EnergytrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.java.energytracker.energytracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnergytrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
